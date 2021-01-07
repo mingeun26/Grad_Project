eaadca9c9524a27a62c50f133f7ea4bfea3bbe2c
Hongjoo22 <dlghdwn10@gmail.com>
2021-01-07 22:15:42 +0900

    캐릭터 이동
    이동,점프,앉기, 달리기 등

---
+++ Assets/Scripts/Interact.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3627a8aecde3f0fd6c590fbba2278eac8e1d492fda7386b962882d6a5114365c
+size 687
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:61cf1867d89ae104076cb7b3adbedc77f15fb2c412ba659f8d89fee4ed8810b7
+size 6585
