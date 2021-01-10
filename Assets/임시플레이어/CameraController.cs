8e9c6412f25719dc7e078419ba2fa1d55a96f574
mingeun26 <76557726+mingeun26@users.noreply.github.com>
2021-01-10 21:56:19 +0900

    1stFloor_BoxCollider
    1층 박스콜라이더 생성 및
    부자연스러운 부분 수정

---
+++ Assets/임시플레이어/CameraController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c91da1a78f04a2ddea0a4219cc6ff1d0a166fd665801d70956c9727670f57c14
+size 1365
---
+++ Assets/임시플레이어/PlayerController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8703c89059cb8807369bee2c3a089b09dc5281817aa4e16e647d47c4ea93a94d
+size 958
