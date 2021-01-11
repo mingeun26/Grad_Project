e76f835afed98817bef9ee38bde7687214b7a117
Hongjoo22 <dlghdwn10@gmail.com>
2021-01-11 23:00:00 +0900

    집구조 콜라이더적용
    2층 및 전체적인 콜라이더적용+움직임 보정

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61cf1867d89ae104076cb7b3adbedc77f15fb2c412ba659f8d89fee4ed8810b7
-size 6585
+oid sha256:312ef02cbb84e56d1046619589c428b4a8acf5c1d60369c5de9f0c20f990386d
+size 6687
