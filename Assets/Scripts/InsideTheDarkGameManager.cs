e575a750879c5ec5932de1f273ac38d965bf73f4
Hongjoo22 <dlghdwn10@gmail.com>
2021-02-01 20:48:46 +0900

    gamescene 최종
    insidethedarek gamemanager 스크립트
    프리팹수정
    프리팹위치수정

--- Assets/Scripts/InsideTheDarkGameManager.cs
+++ Assets/Scripts/InsideTheDarkGameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f0c04049fbfbc61c2b01c9b36540bf6c0cccf5d4d8d931251a3318d60cc3a3f
-size 1987
+oid sha256:8343ab1d20dca20163720297ec18a52c8df2323ff55c7bab2fc7a27b886f3a98
+size 2111
