e3f687dc26ed360a920e04d9b7ab9ce147c16ea8
Hongjoo22 <dlghdwn10@gmail.com>
2021-02-12 14:33:22 +0900

    place modify
    except double jump

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:288c615ce99032bf40bd32fd13444cabcb2e75a5d54ea58c8749c95f27e8c05b
-size 6599
+oid sha256:6a746b67cb9ddc3ddfe740067ced30adfdc9dadd93774d4b89520d1272cf2154
+size 6664
