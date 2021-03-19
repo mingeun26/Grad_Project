4b6236523df77f40ee7d02be96ea43c7cb81ac34
juyeon-park <pjy1107@naver.com>
2021-03-19 22:09:54 +0900

    door key use
    1차

--- Assets/Scripts/AboutItems/Slot.cs
+++ Assets/Scripts/AboutItems/Slot.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7976cf29da3dbd1014412c25c2f3f03d701e9aa33a32213273bba67d8a178f94
-size 1099
+oid sha256:a1c9be9e813a3dcfd435f29c50be124a043358a1ec43021711c99164e307e63b
+size 1363
--- Assets/Scripts/PlayerScripts/ShowThiefPanel.cs
+++ Assets/Scripts/PlayerScripts/ShowThiefPanel.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f180f59db189bc381d6273c9602b7a8c79fd058075d60e76dc4831d8d15884d2
-size 1105
+oid sha256:415db7de29f0087532018ece7c900f0e0f94f5aa5df82456159bac8264c6a407
+size 1222
