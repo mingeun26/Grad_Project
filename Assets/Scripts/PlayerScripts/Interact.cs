829cca47db7aee55172be117f3b5318d14cbdcd9
mingeun26 <76557726+mingeun26@users.noreply.github.com>
2021-04-04 22:50:20 +0900

    DoorPhoton
    Doors are shared + assets bit cleared

--- Assets/Scripts/PlayerScripts/Interact.cs
+++ Assets/Scripts/PlayerScripts/Interact.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a246f95e0d68b6d3583e33b4c1ea7fe11017b42ced4add93302a2c4d310ea957
-size 2409
+oid sha256:5736a766ee6324ce3803a2965079ea2b1bcdcead34af2b578d21e1b1665cad99
+size 2434
