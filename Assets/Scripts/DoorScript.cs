fa088de72e2dd6f1ea52af3368bb7dc79325e7a9
Hongjoo22 <dlghdwn10@gmail.com>
2021-04-30 21:49:15 +0900

    door_final
    sound little

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4c3463aac980cf5979c3232f95b65fca19c25efe6d2e37e85c349215077b267
-size 2627
+oid sha256:d550adf7dfbe92d5bdb556161d2d15caaa8f7c1f16cfcea79effbcc34396487c
+size 2600
