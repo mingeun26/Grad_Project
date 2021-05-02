ae0b4d8b88a9bbce16e24be5637657fcd8787960
Hongjoo22 <dlghdwn10@gmail.com>
2021-05-02 11:21:28 +0900

    sound_final
    기타 버그 수정

--- Assets/Scripts/PlayerScripts/Skill.cs
+++ Assets/Scripts/PlayerScripts/Skill.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:175f04301b54cd93ca2e85ad5759580d3ede9aada0ef4f6a39614bf3686279f8
-size 2054
+oid sha256:36b2c53101c5b53f310515163f73e0dd78b5c112249929a325b9e5b4acc40abd
+size 2304
