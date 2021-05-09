676813881425adec074f06643240f3a12aa0bf78
mingeun26 <76557726+mingeun26@users.noreply.github.com>
2021-05-09 22:49:51 +0900

    FixedBug!
    A lot

---
+++ Assets/Scripts/AboutItems/Destroy.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:23a72b8b3c2990f82700cb27ccabc0d4fcfb3ecca2f6fe5112262522fedbc516
+size 1161
--- Assets/Scripts/InsideTheDarkGameManager.cs
+++ Assets/Scripts/InsideTheDarkGameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c23e1be7a62dbf130b6c2310af0ccf28442ef16e6be77a3b2f10b2d9a6568736
-size 10310
+oid sha256:67e5d2f0f1c105d9b3c9dcbceeb525e3003a5a29bc888eb9738567103a3cb5ab
+size 10012
