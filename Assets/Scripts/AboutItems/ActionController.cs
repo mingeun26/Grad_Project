4dc6af1c05db83c46cc3a8c78a60601de4ccdd47
mingeun26 <76557726+mingeun26@users.noreply.github.com>
2021-05-13 03:57:31 +0900

    2ndBug_text
    text, sound effect, etc.

--- Assets/Scripts/AboutItems/ActionController.cs
+++ Assets/Scripts/AboutItems/ActionController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43a1dace92eab93dd691298de00815be1ee3ba482525f4d6adea2af0f04848c6
-size 12192
+oid sha256:e3d4e44d38b2f34c36709d2b3894c58e361c2c74592564aed0fc68b66f6f67e0
+size 11696
--- Assets/Scripts/InsideTheDarkGameManager.cs
+++ Assets/Scripts/InsideTheDarkGameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f6a042d5123ee31c68a69d8876eb7a8a3d993a0ef0075c30bb8f9c3c5d970ba
-size 10239
+oid sha256:285453fb8ab05fa2ffc1acef9f838cf0ff14133521df080b75656bcfe358ecd6
+size 11499
